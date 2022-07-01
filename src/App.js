3e06390f6cd51c4aed4132544a5fb5185376b4b9
Maxi Giraudo <maximilianoezequielgiraudo@gmail.com>
2022-07-01 19:29:15 -0300

    cambio chau

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-console.log("hola")
+console.log("hola");
+
+console.log("chau");
 
 function App() {
   return (
