ce27b8bc793ef2042ade1d0f56d992840a0784a5
Jonathan Caliva <cjandy.jc@gmail.com>
2022-07-05 19:33:36 -0300

    footer

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
+
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes} from 'react-router-dom';
 import LandingPage from "./components/LandingPage/LandingPage";
 import Home from "./components/Home/Home";
 import Detail from "./components/Detail/Detail";
 import Form from "./components/Form/Form";
 
+
+
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route exact path="/" element={<LandingPage />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/detail" element={<Detail />} />
-          <Route path="/form" element={<Form />} />
-        </Routes>
-      </div>
-    </Router>
+  return (  
+      <Routes>
+        <Route exact path='/' element={<LandingPage/>} />
+        <Route path='/home' element={<Home/>} />
+        <Route path="/detail" element={<Detail/>}/>
+        <Route path="/form" element={<Form/>}/>
+      </Routes> 
   );
 }
 export default App;
--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
+import Footer from "../Footer/Footer";
 
 export default function LandingPage() {
-  return <h1> hola! </h1>;
+  return <h1>hola</h1>
 }
