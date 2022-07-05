00bfc3eec3b0f980ce349f298c7924ece9b39be1
Maxi Giraudo <maximilianoezequielgiraudo@gmail.com>
2022-07-05 11:05:35 -0300

    arreglo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,26 @@
 import React from "react";
 
 export default function Navbar() {
-  return <h1> Navbar </h1>;
+  return (
+    <header>
+      <NavLink to="/home">
+        <li>
+          <h1>Home</h1>
+        </li>
+      </NavLink>
+      <nav>
+        <ul>
+          <div>
+            <li>SOBRE MI </li>
+            <NavLink to="/form">
+              <li>Create</li>
+            </NavLink>
+            <li>Login</li>
+            <li>Sing Up</li>
+            <li>Connect Wallet</li>
+          </div>
+        </ul>
+      </nav>
+    </header>
+  );
 }
