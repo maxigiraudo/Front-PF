953135278bd5f771ccff697ced3c869b08804059
Matias Schmidt <matias.schmidt@outlook.es>
2022-07-05 21:09:05 -0300

    modificaciones

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
-export default function Card() {
-  return <h1> Card </h1>;
+export default function Card({price,description,name,image}) {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h1>{description} </h1>
+      <h1>{price} </h1>
+      <img src={image} alt='nft'/>
+    </div>
+  );
 }
