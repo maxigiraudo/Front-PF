90cf24214c31c88957aa0b88705569ccdbd05c84
Jonathan Caliva <cjandy.jc@gmail.com>
2022-07-06 12:38:05 -0300

    actualizacion de descripcion en reducer

--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -9,44 +9,44 @@ const initialState = {
       image:"https://www.latercera.com/resizer/Jw37YLm2lX8o_akIpM57enxchxA=/arc-anglerfish-arc2-prod-copesa/public/XQP24CTNQND2TBDZ7AQN54D44Y.jpg"
     },
     {
-      name: "mono2",
-      description:"detalle2",
+      name: "MONO2",
+      description:"CONSEGUI TODAS LAS EXCLUSIVIDADES CON ESTE MONITO QUE ES CARISIMO",
       price:2,
       image:"https://cdn.decrypt.co/resize/1536/wp-content/uploads/2021/10/bored-ape-nft-sothebys-record-sale-gID_4.png.webp"
     },
     {
-      name: "mono3",
-      description:"detalle3",
+      name: "MONO3",
+      description:"CONSEGUI TODAS LAS EXCLUSIVIDADES CON ESTE MONITO QUE ES CARISIMO",
       price:3,
       image:"https://planetanft.com/wp-content/uploads/2021/12/Bored-Ape-Yacht-Club-3.png"
     },
     {
-      name: "mono4",
-      description:"detalle4",
+      name: "MONO4",
+      description:"CONSEGUI TODAS LAS EXCLUSIVIDADES CON ESTE MONITO QUE ES CARISIMO",
       price:4,
       image:"https://assets.iproup.com/cdn-cgi/image/w=880,f=webp/https://assets.iproup.com/assets/jpg/2021/09/22436.jpg"
     },
     {
-      name: "mono5",
-      description:"detalle5",
+      name: "MONO5",
+      description:"CONSEGUI TODAS LAS EXCLUSIVIDADES CON ESTE MONITO QUE ES CARISIMO",
       price:5,
       image:"https://www.larazon.es/resizer/lPwjF6eG-PeAfrCUPPt75lJXgb0=/1800x1200/smart/filters:format(webp):quality(65)/cloudfront-eu-central-1.images.arcpublishing.com/larazon/C3QPAGIE3RBULNY6TPH7GGWBTQ.jpg"
     },
     {
-      name: "mono6",
-      description:"detalle6",
+      name: "MONO6",
+      description:"CONSEGUI TODAS LAS EXCLUSIVIDADES CON ESTE MONITO QUE ES CARISIMO",
       price:6,
       image:"https://assets.iproup.com/cdn-cgi/image/w=880,f=webp/https://assets.iproup.com/assets/jpg/2021/09/22436.jpg"
     },
     {
-      name: "mono7",
-      description:"detalle7",
+      name: "MONO7",
+      description:"CONSEGUI TODAS LAS EXCLUSIVIDADES CON ESTE MONITO QUE ES CARISIMO",
       price:7,
       image:"https://pbs.twimg.com/media/FJ-piSWX0AMTw5U.png"
     },
     {
-      name: "mono8",
-      description:"detalle8",
+      name: "MONO8",
+      description:"CONSEGUI TODAS LAS EXCLUSIVIDADES CON ESTE MONITO QUE ES CARISIMO",
       price:8,
       image:"https://www.publimetro.com.mx/resizer/CocGeimTnro-D1hoH3ZC1Nrs1IY=/arc-photo-metroworldnews/arc2-prod/public/RSNJQRDEKNBMRC3Z4SYV64SZT4.jpg"
     }
