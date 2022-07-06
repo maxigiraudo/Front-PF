a13f73fa4fe200a30bae2b8a2a6b378317ec4b8e
Jonathan Caliva <cjandy.jc@gmail.com>
2022-07-06 10:23:55 -0300

    otro

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,29 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import Footer from "../Footer/Footer";
 import Navbar from "../Navbar/Navbar";
 import Searchbar from "../Searchbar/Searchbar";
+import Card from "../Card/Card"
 
 export default function Home() {
+  const currentCard = useSelector((state)=> state.cards)
   return (
     <>
       <Navbar />
       <Searchbar />
+      <div>{currentCard?.map((e)=>
+      <Card
+        price={e.price}
+        name={e.name}
+        description={e.description}
+        image={e.image}
+      />
+      )
+      }
+      </div>
+      
       <Footer />
+
     </>
+  )
 }
--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -1,4 +1,55 @@
 const initialState = {
-  cards: [],
+  cards: [
+    {
+      name: "mono1",
+      description:"detalle1",
+      price:1,
+      image:"https://www.latercera.com/resizer/Jw37YLm2lX8o_akIpM57enxchxA=/arc-anglerfish-arc2-prod-copesa/public/XQP24CTNQND2TBDZ7AQN54D44Y.jpg"
+    },
+    {
+      name: "mono2",
+      description:"detalle2",
+      price:2,
+      image:"https://cdn.decrypt.co/resize/1536/wp-content/uploads/2021/10/bored-ape-nft-sothebys-record-sale-gID_4.png.webp"
+    },
+    {
+      name: "mono3",
+      description:"detalle3",
+      price:3,
+      image:"https://planetanft.com/wp-content/uploads/2021/12/Bored-Ape-Yacht-Club-3.png"
+    },
+    {
+      name: "mono4",
+      description:"detalle4",
+      price:4,
+      image:"https://assets.iproup.com/cdn-cgi/image/w=880,f=webp/https://assets.iproup.com/assets/jpg/2021/09/22436.jpg"
+    },
+    {
+      name: "mono5",
+      description:"detalle5",
+      price:5,
+      image:"https://www.larazon.es/resizer/lPwjF6eG-PeAfrCUPPt75lJXgb0=/1800x1200/smart/filters:format(webp):quality(65)/cloudfront-eu-central-1.images.arcpublishing.com/larazon/C3QPAGIE3RBULNY6TPH7GGWBTQ.jpg"
+    },
+    {
+      name: "mono6",
+      description:"detalle6",
+      price:6,
+      image:"https://assets.iproup.com/cdn-cgi/image/w=880,f=webp/https://assets.iproup.com/assets/jpg/2021/09/22436.jpg"
+    },
+    {
+      name: "mono7",
+      description:"detalle7",
+      price:7,
+      image:"https://pbs.twimg.com/media/FJ-piSWX0AMTw5U.png"
+    },
+    {
+      name: "mono8",
+      description:"detalle8",
+      price:8,
+      image:"https://www.publimetro.com.mx/resizer/CocGeimTnro-D1hoH3ZC1Nrs1IY=/arc-photo-metroworldnews/arc2-prod/public/RSNJQRDEKNBMRC3Z4SYV64SZT4.jpg"
+    }
+  ],
 };
-export default function reducer(state = initialState, action) {}
+export default function reducer(state = initialState, action) {
+  return state
+}
