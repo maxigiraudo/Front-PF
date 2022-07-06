6b2a85654671f552caa9452297d6077c72023e13
Maxi Giraudo <maximilianoezequielgiraudo@gmail.com>
2022-07-06 14:43:25 -0300

    search

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,35 +2,32 @@ import React from "react";
 import { useSelector } from "react-redux";
 import Footer from "../Footer/Footer";
 import Navbar from "../Navbar/Navbar";
-import Searchbar from "../Searchbar/Searchbar";
-import Card from "../Card/Card"
-import style from "./Home.module.css"
+
+import Card from "../Card/Card";
+import style from "./Home.module.css";
 
 export default function Home() {
-  const currentCard = useSelector((state)=> state.cards)
-  
+  const currentCard = useSelector((state) => state.cards);
+
   return (
     <>
       <Navbar />
-      <Searchbar />
-      <div className="row gap-2 justify-center" >
-        <div className={style.cartoneta} >{currentCard?.map((e)=>
-          <Card
-            price={e.price}
-            name={e.name}
-            description={e.description}
-            image={e.image}
-            created={e.created}
-          />
-          )
-          }
+
+      <div className="row gap-2 justify-center">
+        <div className={style.cartoneta}>
+          {currentCard?.map((e) => (
+            <Card
+              price={e.price}
+              name={e.name}
+              description={e.description}
+              image={e.image}
+              created={e.created}
+            />
+          ))}
         </div>
       </div>
-      
+
       <Footer />
-
     </>
-  )
-
+  );
 }
-
