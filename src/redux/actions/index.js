d3080b5f1674a8fc961a26a0d44d981abd904b08
Jonathan Caliva <cjandy.jc@gmail.com>
2022-07-17 16:14:41 -0300

    no es nada

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,5 +1,6 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import Moralis from "moralis";
+import { MdCallToAction } from "react-icons/md";
 import Swal from "sweetalert2";
 
 export function orderByPrice(payload) {
@@ -142,12 +143,12 @@ export function createAcount({ nombre, email, password }) {
         type: "CREATE_ACOUNT_SUCCESS",
         payload: json.data,
       });
-      window.location.href = "http://localhost:3000/login"
+      window.location.href = "https://wallaby-neon.vercel.app/login";
     } catch (error) {
       Swal.fire({
         icon: "error",
         text: "Email existent",
-        showConfirmButton: true
+        showConfirmButton: true,
       });
       console.log("ESTE ES EL ERROR", error);
     }
@@ -165,7 +166,7 @@ export function getSliderNftArt() {
   return async function (dispatch) {
     try {
       let json = await axios.get("http://localhost:4000/api/nftcollection");
-      console.log("ESTA ES LA COLECCION",json);
+      console.log("ESTA ES LA COLECCION", json);
       return dispatch({
         type: "GET_SLIDER_NFT",
 
@@ -212,6 +213,7 @@ export function contador(contador) {
   };
 }
 export function postLogin(payload) {
+  console.log("normla loguin f" + payload);
   return async function (dispatch) {
     axios
       .post("https://henry-proyecto-nft.herokuapp.com/api/login", payload)
@@ -227,9 +229,18 @@ export function postLogin(payload) {
           showConfirmButton: false,
           timer: 1500,
         });
+        // window.location.href = "http://localhost:3000/home";
         console.log("logueado");
       })
       .catch((err) => {
+        if (err.response.status === 400) {
+          Swal.fire({
+            icon: "error",
+            text: "Email or password incorrect or inexistent",
+            showConfirmButton: true,
+          });
+        }
+
         console.log(err);
       });
   };
@@ -248,3 +259,116 @@ export const addFavorite = (info) => {
     payload: info,
   };
 };
+
+export const register = (body) => async (dispatch) => {
+  // try {
+  const newbody = { email: body.email, password: body.password };
+  console.log("este es el bodyyyyy", body);
+  dispatch({
+    type: "REGISTER_USER_REQUEST",
+  });
+  const config = {
+    headers: { "Content-Type": "application/json" },
+  };
+  try {
+    const { data } = await axios.post(
+      "https://henry-proyecto-nft.herokuapp.com/auth/api/signup",
+      body,
+      config
+    );
+    dispatch({
+      type: "USER_LOGIN_SUCCESS",
+      payload: data,
+    });
+  } catch (err) {
+    const errorString = String(err).slice(0, 10);
+    if (errorString === "AxiosError") {
+      console.log("VAMOOOOOOOOOOOOO");
+      body = newbody;
+      console.log("nuevo bodyyy", body);
+      const { data } = await axios.post(
+        "https://henry-proyecto-nft.herokuapp.com/api/signin",
+        body,
+        config
+      );
+      dispatch({
+        type: "REGISTER_USER_SUCCESS",
+        payload: data,
+      });
+      console.log("LOGUEADO TITANN", data);
+      // Swal("Registro Exitoso",{icon:"success"});
+      // window.location.href = "/home";
+    } else {
+      return console.log(err);
+    }
+  }
+
+  // console.log("esta es la dataaa",data)
+  // if(data.user.email) {
+
+  // } else{
+
+  // }
+
+  // } catch (error) {
+  //   Swal("Credenciales Incorrectas", { icon: "warning" });
+  //   dispatch({
+  //     type: 'REGISTER_USER_ERROR',
+  //     payload: error,
+  //   });
+  // }
+};
+
+export const login =
+  ({ email }) =>
+  async (dispatch) => {
+    try {
+      dispatch({
+        type: "USER_LOGIN_REQUEST",
+      });
+
+      const data = await axios.post(`http://localhost:4000/auth/api/signin`, {
+        email,
+        // password,
+      });
+      console.log("choclochoclo", data.request);
+      switch (data.request.status) {
+        case 200:
+          dispatch({
+            type: "USER_LOGIN_SUCCESS",
+            payload: data.data,
+          });
+          localStorage.setItem("userInfo", JSON.stringify(data));
+          window.location.href = "/home";
+          break;
+        case 401:
+          dispatch({
+            type: "USER_LOGIN_ERROR",
+            payload: data.error,
+          });
+          Swal("Not allow", { icon: "warning" });
+          break;
+        case 500:
+          dispatch({
+            type: "USER_LOGIN_ERROR",
+            payload: data.error,
+          });
+          Swal("Internal server error", { icon: "warning" });
+          break;
+        default:
+          break;
+      }
+    } catch (error) {
+      Swal("Credenciales Incorrectas", { icon: "warning" });
+      dispatch({
+        type: "USER_LOGIN_ERROR",
+        payload: error,
+      });
+    }
+  };
+
+export function singoutOk() {
+  return {
+    type: "SINGOUT_OK",
+  };
+}
