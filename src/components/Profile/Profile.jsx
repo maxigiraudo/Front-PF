6a08e82b555c102d50622f2db15be4a2958153c0
Jonathan Caliva <cjandy.jc@gmail.com>
2022-07-18 13:57:40 -0300

    seguimos probando google

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -26,6 +26,7 @@ export default function Profile(){
     const profile = useSelector((state)=>state.profile)
     const profileGoogle = useSelector((state)=> state.profileGoogle)
     console.log(profile)
+    console.log(profileGoogle)
 
 
   return (
