52292461c335f5e0107fcf02e1ff852e12219468
Jonathan Caliva <cjandy.jc@gmail.com>
2022-07-22 20:37:09 -0300

    ahora siiiiiii Cristian Lair

--- src/App.js
+++ src/App.js
@@ -180,7 +180,7 @@ function App() {
         <Route path="/mycollections" element={<MyCollections />} />
         <Route path="/myorders" element={<MyOrders />} />
 
-        <Route path="/:email/recoverpassword" element={<RecoverPassword/>} />
+        <Route path="/:email/newpassword" element={<RecoverPassword/>} />
 
         <Route path="/star" element={<Star />} />
 
