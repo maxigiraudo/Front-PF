c3ab6a71f336de41913b30b79e03ec4e22fe4e28
Maxi Giraudo <maximilianoezequielgiraudo@gmail.com>
2022-07-26 01:04:20 -0300

    chau polify chau

--- src/polyfill.js
+++ src/polyfill.js
@@ -1,50 +1,50 @@
-import { Buffer } from "buffer";
-const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+// import { Buffer } from "buffer";
+// const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 
-window.global = window;
-global.Buffer = Buffer;
-global.process = {
-  env: { DEBUG: undefined },
-  version: "",
-  nextTick: require("next-tick"),
+// window.global = window;
+// global.Buffer = Buffer;
+// global.process = {
+//   env: { DEBUG: undefined },
+//   version: "",
+//   nextTick: require("next-tick"),
 
-  //   resolve: {
-  //     fallback: {
-  //       fs: false,
-  //       tls: false,
-  //       net: false,
-  //       path: false,
-  //       zlib: false,
-  //       http: false,
-  //       https: false,
-  //       stream: false,
-  //       crypto: false,
-  //     },
-  //   },
-  plugins: [new NodePolyfillPlugin()],
-};
+//   //   resolve: {
+//   //     fallback: {
+//   //       fs: false,
+//   //       tls: false,
+//   //       net: false,
+//   //       path: false,
+//   //       zlib: false,
+//   //       http: false,
+//   //       https: false,
+//   //       stream: false,
+//   //       crypto: false,
+//   //     },
+//   //   },
+//   plugins: [new NodePolyfillPlugin()],
+// };
 
-// function override(config) {
-//   const fallback = config.resolve.fallback || {};
-//   Object.assign(fallback, {
-//     crypto: require.resolve("crypto-browserify"),
-//     stream: require.resolve("stream-browserify"),
-//     assert: require.resolve("assert"),
-//     https: require.resolve("https-browserify"),
-//     os: require.resolve("os-browserify"),
-//     url: require.resolve("url"),
-//     // http: require.resolve("stream-http"),
-//   });
-//   config.resolve.fallback = fallback;
-//   config.plugins = (config.plugins || []).concat([
-//     new webpack.ProvidePlugin({
-//       process: "process/browser",
-//       Buffer: ["buffer", "Buffer"],
-//       plugins: [new Dotenv()],
-//     }),
-//   ]);
-//   console.log(config);
-//   return config;
-// }
+// // function override(config) {
+// //   const fallback = config.resolve.fallback || {};
+// //   Object.assign(fallback, {
+// //     crypto: require.resolve("crypto-browserify"),
+// //     stream: require.resolve("stream-browserify"),
+// //     assert: require.resolve("assert"),
+// //     https: require.resolve("https-browserify"),
+// //     os: require.resolve("os-browserify"),
+// //     url: require.resolve("url"),
+// //     // http: require.resolve("stream-http"),
+// //   });
+// //   config.resolve.fallback = fallback;
+// //   config.plugins = (config.plugins || []).concat([
+// //     new webpack.ProvidePlugin({
+// //       process: "process/browser",
+// //       Buffer: ["buffer", "Buffer"],
+// //       plugins: [new Dotenv()],
+// //     }),
+// //   ]);
+// //   console.log(config);
+// //   return config;
+// // }
 
-// config.ignoreWarnings = [/Failed to parse source map/];
+// // config.ignoreWarnings = [/Failed to parse source map/];
