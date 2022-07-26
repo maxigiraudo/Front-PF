4cb3006cf35a5457448857641bf06e7f1ccd2b25
Maxi Giraudo <maximilianoezequielgiraudo@gmail.com>
2022-07-26 02:03:50 -0300

    otro push

--- src/polyfill.js
+++ src/polyfill.js
@@ -11,14 +11,14 @@ global.process = {
     console.log("override");
     let loaders = config.resolve;
     loaders.fallback = {
-      path: require.resolve("path-browserify"),
-      os: require.resolve("os-browserify/browser"),
-      crypto: require.resolve("crypto-browserify"),
-      stream: require.resolve("stream-browserify"),
-      assert: require.resolve("assert"),
-      http: require.resolve("stream-http"),
-      https: require.resolve("https-browserify"),
-      url: require.resolve("url"),
+      //   path: require.resolve("path-browserify"),
+      //   os: require.resolve("os-browserify/browser"),
+      //   crypto: require.resolve("crypto-browserify"),
+      //   stream: require.resolve("stream-browserify"),
+      //   assert: require.resolve("assert"),
+      http: false,
+      //   https: require.resolve("https-browserify"),
+      //   url: require.resolve("url"),
     };
 
     return config;
