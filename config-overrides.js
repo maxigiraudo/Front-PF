7a57a210841dc0f9db9bf72eed16ac1fd0fe91a0
Maxi Giraudo <maximilianoezequielgiraudo@gmail.com>
2022-07-26 01:36:43 -0300

    holaadios

--- config-overrides.js
+++ config-overrides.js
@@ -1,38 +1,17 @@
-// module.exports = function override(config, env) {
-//   console.log("override");
-//   let loaders = config.resolve;
-//   loaders.fallback = {
-//     path: require.resolve("path-browserify"),
-//     os: require.resolve("os-browserify/browser"),
-//     "crypto": require.resolve("crypto-browserify"),
-//     "stream": require.resolve("stream-browserify"),
-//     "assert": require.resolve("assert"),
-//     "http": require.resolve("stream-http"),
-//     "https": require.resolve("https-browserify"),
-//     "url": require.resolve("url"),
-//     buffer : require.resolve('buffer/')
-//   };
-
-//   return config;
-// };
-const webpack = require("webpack");
-module.exports = function override(config) {
-  const fallback = config.resolve.fallback || {};
-  Object.assign(fallback, {
+module.exports = function override(config, env) {
+  console.log("override");
+  let loaders = config.resolve;
+  loaders.fallback = {
+    path: require.resolve("path-browserify"),
+    os: require.resolve("os-browserify/browser"),
     crypto: require.resolve("crypto-browserify"),
     stream: require.resolve("stream-browserify"),
     assert: require.resolve("assert"),
     http: require.resolve("stream-http"),
     https: require.resolve("https-browserify"),
-    os: require.resolve("os-browserify"),
     url: require.resolve("url"),
-  });
-  config.resolve.fallback = fallback;
-  config.plugins = (config.plugins || []).concat([
-    new webpack.ProvidePlugin({
-      process: "process/browser",
-      Buffer: ["buffer", "Buffer"],
-    }),
-  ]);
+    buffer: require.resolve("buffer/"),
+  };
+
   return config;
 };
--- src/polyfill.js
+++ src/polyfill.js
@@ -1,71 +1,9 @@
-// import { Buffer } from "buffer";
-// const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+import { Buffer } from "buffer";
 
-// window.global = window;
-// global.Buffer = Buffer;
-// global.process = {
-//   env: { DEBUG: undefined },
-//   version: "",
-//   nextTick: require("next-tick"),
-
-//   //   resolve: {
-//   //     fallback: {
-//   //       fs: false,
-//   //       tls: false,
-//   //       net: false,
-//   //       path: false,
-//   //       zlib: false,
-//   //       http: false,
-//   //       https: false,
-//   //       stream: false,
-//   //       crypto: false,
-//   //     },
-//   //   },
-//   plugins: [new NodePolyfillPlugin()],
-// };
-
-// function override(config) {
-//   const fallback = config.resolve.fallback || {};
-//   Object.assign(fallback, {
-//     crypto: require.resolve("crypto-browserify"),
-//     stream: require.resolve("stream-browserify"),
-//     assert: require.resolve("assert"),
-//     https: require.resolve("https-browserify"),
-//     os: require.resolve("os-browserify"),
-//     url: require.resolve("url"),
-//     // http: require.resolve("stream-http"),
-//   });
-//   config.resolve.fallback = fallback;
-//   config.plugins = (config.plugins || []).concat([
-//     new webpack.ProvidePlugin({
-//       process: "process/browser",
-//       Buffer: ["buffer", "Buffer"],
-//       plugins: [new Dotenv()],
-//     }),
-//   ]);
-//   console.log(config);
-//   return config;
-// }
-
-// // config.ignoreWarnings = [/Failed to parse source map/];
-const webpack = require("webpack");
-module.exports = function override(config) {
-  const fallback = config.resolve.fallback || {};
-  Object.assign(fallback, {
-    crypto: require.resolve("crypto-browserify"),
-    stream: require.resolve("stream-browserify"),
-    assert: require.resolve("assert"),
-    http: require.resolve("stream-http"),
-    https: require.resolve("https-browserify"),
-    os: require.resolve("os-browserify"),
-    url: require.resolve("url"),
-  });
-  config.resolve.fallback = fallback;
-  config.plugins = (config.plugins || []).concat([
-    new webpack.ProvidePlugin({
-      process: "process/browser",
-      Buffer: ["buffer", "Buffer"],
-    }),
-  ]);
-  return config;
+window.global = window;
+global.Buffer = Buffer;
+global.process = {
+  env: { DEBUG: undefined },
+  version: "",
+  nextTick: require("next-tick"),
 };
