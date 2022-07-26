08504cd1a8e52f251001e7dc5baaa74d8d834518
Maxi Giraudo <maximilianoezequielgiraudo@gmail.com>
2022-07-26 00:13:46 -0300

    asfsdf

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,31 @@
+const webpack = require("webpack");
 const Dotenv = require("dotenv-webpack");
-module.exports = {
-  resolve: {
-    fallback: {
-      fs: false,
-      tls: false,
-      net: false,
-      path: false,
-      zlib: false,
-      http: false,
-      https: false,
-      stream: false,
-      crypto: false,
-      "crypto-browserify": require.resolve("crypto-browserify"), //if you want to use this module also don't forget npm i crypto-browserify
-    },
-  },
 
-  plugins: [
-    new Dotenv(),
+// module.exports = {
+//   plugins: [
+//     new Dotenv(),
+//   ],
+// };
+
+module.exports = function override(config) {
+  const fallback = config.resolve.fallback || {};
+  Object.assign(fallback, {
+    crypto: require.resolve("crypto-browserify"),
+    stream: require.resolve("stream-browserify"),
+    assert: require.resolve("assert"),
+    http: require.resolve("stream-http"),
+    https: require.resolve("https-browserify"),
+    os: require.resolve("os-browserify"),
+    url: require.resolve("url"),
+  });
+  config.resolve.fallback = fallback;
+  config.plugins = (config.plugins || []).concat([
     new webpack.ProvidePlugin({
+      process: "process/browser",
       Buffer: ["buffer", "Buffer"],
+      plugins: [new Dotenv()],
     }),
-  ],
+  ]);
+
+  return config;
 };
