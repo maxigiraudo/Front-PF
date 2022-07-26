da53c8781706d07045728e7bc39830e146352be2
Maxi Giraudo <maximilianoezequielgiraudo@gmail.com>
2022-07-26 00:06:25 -0300

    arreglando

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,16 @@ const Dotenv = require("dotenv-webpack");
 module.exports = {
   resolve: {
     fallback: {
-      crypto: require.resolve("crypto-browserify"),
+      fs: false,
+      tls: false,
+      net: false,
+      path: false,
+      zlib: false,
+      http: false,
+      https: false,
       stream: false,
+      crypto: false,
+      "crypto-browserify": require.resolve("crypto-browserify"), //if you want to use this module also don't forget npm i crypto-browserify
     },
   },
 
