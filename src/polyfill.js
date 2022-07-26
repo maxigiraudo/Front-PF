7179687ac8e0084de478c0c67ff9a87d595d9b93
Maxi Giraudo <maximilianoezequielgiraudo@gmail.com>
2022-07-26 02:00:22 -0300

    cristian lair creado

--- src/polyfill.js
+++ src/polyfill.js
@@ -6,4 +6,38 @@ global.process = {
   env: { DEBUG: undefined },
   version: "",
   nextTick: require("next-tick"),
+
+  override(config, env) {
+    console.log("override");
+    let loaders = config.resolve;
+    loaders.fallback = {
+      path: require.resolve("path-browserify"),
+      os: require.resolve("os-browserify/browser"),
+      crypto: require.resolve("crypto-browserify"),
+      stream: require.resolve("stream-browserify"),
+      assert: require.resolve("assert"),
+      http: require.resolve("stream-http"),
+      https: require.resolve("https-browserify"),
+      url: require.resolve("url"),
+    };
+
+    return config;
+  },
 };
+
+// module.exports = function override(config, env) {
+//   console.log("override");
+//   let loaders = config.resolve;
+//   loaders.fallback = {
+//     path: require.resolve("path-browserify"),
+//     os: require.resolve("os-browserify/browser"),
+//     crypto: require.resolve("crypto-browserify"),
+//     stream: require.resolve("stream-browserify"),
+//     assert: require.resolve("assert"),
+//     http: require.resolve("stream-http"),
+//     https: require.resolve("https-browserify"),
+//     url: require.resolve("url"),
+//   };
+
+//   return config;
+// };
